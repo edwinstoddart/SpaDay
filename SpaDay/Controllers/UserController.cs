dc2a102ebcf469e5a4e82e3a8240948a70d4346f
Edwin DeJesus <edwinstoddart.student@gmail.com>
2021-11-01 18:02:54 -0400

    Completed The assignment and bonus mission.

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /user/add
+        public IActionResult Add()
+        {
+            return View();
+        }
+        // POST: /user
+        [HttpPost("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string username, string email, string verify)
+        {
+            if (!String.IsNullOrWhiteSpace(verify) && verify.Equals(newUser.Password))
+            {
+                ViewBag.newUser = newUser;
+                return View("Index");
+            } else
+            {
+                ViewBag.passwordMismatch = true;
+                ViewBag.Username = username;
+                ViewBag.Email = email;
+                return View("Add");
+            }
+        }
+    }
+}
